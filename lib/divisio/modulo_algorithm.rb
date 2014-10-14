5c5cbbf2ddc48a6b663d869ccdd8aaf5067f990c
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-10-14 16:33:19 +0100

    tests for modulo algorithm

--- lib/divisio/modulo_algorithm.rb
+++ lib/divisio/modulo_algorithm.rb
@@ -9,11 +9,11 @@ class Divisio
 
     def initialize(key, variants)
       @key = key
-      @variants = variants
+      @variants = Array(variants)
     end
 
     def calc
-      number_of_variants = Array(variants).size
+      number_of_variants = variants.size
       position = Digest::MD5.hexdigest(key).to_i(16) % number_of_variants
 
       variants[position]
