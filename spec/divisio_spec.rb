e80c8bc77e786bf5b7385f6ea5ba50d58ef8a424
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-10-29 14:12:55 +0000

    normalizing identity

--- spec/divisio_spec.rb
+++ spec/divisio_spec.rb
@@ -1,50 +1,59 @@
-require 'spec_helper' do
-  describe Divisio do
-    describe '::default_adapter' do
-      it "has a default adapter" do
-        expect(Divisio.default_adapter).to eq(Divisio::TestAdapter)
+require 'spec_helper'
+
+describe Divisio do
+  describe '::default_adapter' do
+    it "has a default adapter" do
+      expect(Divisio.default_adapter).to eq(Divisio::TestAdapter)
+    end
+  end
+
+  describe '::default_adapter=' do
+    it "overwrites the default adapter" do
+      Divisio.default_adapter = Divisio::MongoidAdapter
+      expect(Divisio.default_adapter).to eq(Divisio::MongoidAdapter)
+    end
+  end
+
+  describe '::new' do
+    context 'no adapter provided to initializer' do
+      it "initializes an object with a default adapter" do
+        framework = Divisio.new
+
+        expect(framework.instance_variable_get("@adapter")).to eq(Divisio.default_adapter)
       end
     end
 
-    describe '::default_adapter=' do
-      it "overwrites the default adapter" do
-        Divisio.default_adapter = Divisio::MongoidAdapter
-        expect(Divisio.default_adapter).to eq(Divisio::MongoidAdapter)
+    context 'adapter provided to initializer' do
+      it "initializes an object with a supplied adapter" do
+        framework = Divisio.new(adapter: Divisio::MongoidAdapter)
+
+        expect(framework.instance_variable_get("@adapter")).to eq(Divisio::MongoidAdapter)
+      end
+    end
+  end
+
+  describe '#split' do
+    it "calls the same method on the adapter" do
+      expect(Divisio::MongoidAdapter).to receive(:split)
+
+      Divisio.new(adapter: Divisio::MongoidAdapter).split(anything, anything, anything)
+    end
+
+    context "identity normalization" do
+      it "downcases and removes whitespaces from identity" do
+        expect(Divisio::MongoidAdapter).to receive(:split).with(anything, anything, "testtest")
+
+        Divisio.new(adapter: Divisio::MongoidAdapter).split(anything, anything, "Test Test  ")
       end
     end
 
-    describe '::new' do
-      context 'no adapter provided to initializer' do
-        it "initializes an object with a default adapter" do
-          framework = Divisio.new
+  end
 
-          expect(framework.instance_variable_get("@adapter")).to eq(Divisio.default_adapter)
-        end
-      end
+  describe '#delete_experiment_for_identity' do
+    it "calls the same method on the adapter" do
+      expect(Divisio::MongoidAdapter).to receive(:delete_experiment_for_identity)
 
-      context 'adapter provided to initializer' do
-        it "initializes an object with a supplied adapter" do
-          framework = Divisio.new(adapter: Divisio::MongoidAdapter)
-
-          expect(framework.instance_variable_get("@adapter")).to eq(Divisio::MongoidAdapter)
-        end
-      end
-    end
-
-    describe '#split' do
-      it "calls the same method on the adapter" do
-        expect(Divisio::MongoidAdapter).to receive(:split)
-
-        Divisio.new(adapter: Divisio::MongoidAdapter).split(anything, anything, anything)
-      end
-    end
-
-    describe '#delete_experiment_for_identity' do
-      it "calls the same method on the adapter" do
-        expect(Divisio::MongoidAdapter).to receive(:delete_experiment_for_identity)
-
-        Divisio.new(adapter: Divisio::MongoidAdapter).delete_experiment_for_identity(anything, anything)
-      end
+      Divisio.new(adapter: Divisio::MongoidAdapter).delete_experiment_for_identity(anything, anything)
     end
   end
 end
